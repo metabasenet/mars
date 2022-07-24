250d250c69996e04a77b2262b3e281d130d9c002
zhgu999 <zhgu999@outlook.com>
2022-07-24 20:49:52 +0800

    update function

--- lib/modules/wallet/domain/utils/wallet_path_utils.dart
+++ lib/modules/wallet/domain/utils/wallet_path_utils.dart
@@ -3,7 +3,7 @@ part of wallet_domain_module;
 class WalletPathUtils {
   static final pathConfigs = [
     WalletPathConfig(
-      name: 'Orange',
+      name: 'Passionfruit',
       logo: 'assets/images/wallet_mars.png',
     ),
     WalletPathConfig(
